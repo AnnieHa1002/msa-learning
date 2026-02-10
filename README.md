8411df1b56645d064ce8ae8cb56798b623b7dbb1
하물 <108536712+AnnieHa1002@users.noreply.github.com>
2026-02-10 14:07:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# msa-learning
+learning msa
---
+++ src/main/java/com/sparta/msa_learning/MsaLearningApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaLearningApplication.class, args);
+	}
+
+}
