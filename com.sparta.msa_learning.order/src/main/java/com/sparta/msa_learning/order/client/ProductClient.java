f84e72722d1602b7047fd493a4f36527e9736335
annieha <hajh5447@naver.com>
2026-02-11 14:46:54 +0900

    [feature] ProductClient

---
+++ com.sparta.msa_learning.order/src/main/java/com/sparta/msa_learning/order/client/ProductClient.java
@@ -0,0 +1,15 @@
+package com.sparta.msa_learning.order.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="product-service")
+public interface ProductClient {
+
+    @GetMapping("/api/products")
+    String getProducts();
+
+    @GetMapping("/api/products/{id}")
+    String getProductById(@PathVariable Long id);
+}
