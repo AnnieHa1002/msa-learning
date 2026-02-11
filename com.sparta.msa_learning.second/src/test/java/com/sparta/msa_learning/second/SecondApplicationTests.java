419f4369fb54bb40a8570d43698178836485c3e7
annieha <hajh5447@naver.com>
2026-02-11 12:21:56 +0900

    [feature] set eureka test projects

---
+++ com.sparta.msa_learning.second/src/test/java/com/sparta/msa_learning/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.msa_learning.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ com.sparta.msa_learning.server/src/main/java/com/sparta/msa_learning/MsaLearningApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.msa_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MsaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaLearningApplication.class, args);
+	}
+
+}
