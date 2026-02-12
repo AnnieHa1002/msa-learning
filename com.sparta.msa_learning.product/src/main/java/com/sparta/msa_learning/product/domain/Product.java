3b13d4aaa39c093786b161fc71830cac3354646c
annieha <hajh5447@naver.com>
2026-02-12 13:00:04 +0900

    [feature] project entity

---
+++ com.sparta.msa_learning.product/src/main/java/com/sparta/msa_learning/product/domain/Product.java
@@ -0,0 +1,16 @@
+package com.sparta.msa_learning.product.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.intellij.lang.annotations.Identifier;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long id;
+    private String name;
+    private int price;
+}
