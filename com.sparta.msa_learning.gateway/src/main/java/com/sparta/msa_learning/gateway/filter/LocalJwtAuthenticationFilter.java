7232adedd0fbc826039b6547e43ba8d2de7c31bd
annieha <hajh5447@naver.com>
2026-02-13 14:07:49 +0900

    [feature] security setting

--- com.sparta.msa_learning.gateway/src/main/java/com/sparta/msa_learning/gateway/filter/LocalJwtAuthenticationFilter.java
+++ com.sparta.msa_learning.gateway/src/main/java/com/sparta/msa_learning/gateway/filter/LocalJwtAuthenticationFilter.java
@@ -8,21 +8,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import javax.crypto.SecretKey;
 
 @Slf4j
-
+@Component
 public class LocalJwtAuthenticationFilter implements GlobalFilter {
 
-    @Value("${service.jwt.secret}")
+    @Value("${service.jwt.secret-key}")
     private String jwtSecret;
 
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.startsWith("/api/auth")) {
+            return chain.filter(exchange);
+        }
+
         String token = extractToken(exchange);
         if(token == null || !validateToken(token)) {
             log.warn("### Invalid or missing JWT token");
