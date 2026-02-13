685bf05372f9f2bc9df7b759dd4bb76a3d8bafb8
annieha <hajh5447@naver.com>
2026-02-13 12:40:32 +0900

    [feature] auth-server added to api gateway

---
+++ com.sparta.msa_learning.gateway/src/main/java/com/sparta/msa_learning/gateway/filter/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.sparta.msa_learning.gateway.filter;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret}")
+    private String jwtSecret;
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = extractToken(exchange);
+        if(token == null || !validateToken(token)) {
+            log.warn("### Invalid or missing JWT token");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest()
+                .getHeaders()
+                .getFirst("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            SecretKey secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            log.info("### payloads : {}",
+                    Jwts.parser()
+                            .verifyWith(secretKey)
+                            .build()
+                            .parseSignedClaims(token)
+                            .getPayload()
+            );
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
